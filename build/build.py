fd30689793f8f0911b40060abb768a7848e1809c
wyatthoho <wyatthoho@gmail.com>
2025-02-19 15:45:51 +0800

    refactor: clean code using pathlib

--- build/build.py
+++ build/build.py
@@ -1,19 +1,16 @@
-import os
+from pathlib import Path
 
 import buildhtml
 
 
-def get_md_paths(src_path: str = '.\\markdown') -> list:
-    md_paths = [
-        os.path.join(src_path, path)
-        for path in os.listdir(src_path) if path.endswith('.md')
-    ]
-    return md_paths
+MD_DIR = 'markdown'
+MD_PATTERN = '*.md'
 
 
 def main() -> None:
-    md_paths = get_md_paths()
-    buildhtml.main(md_paths)
+    md_paths = Path(MD_DIR).glob(MD_PATTERN)
+    for path in md_paths:
+        buildhtml.main(path)
 
 
 if __name__ == '__main__':
--- build/buildhtml.py
+++ build/buildhtml.py
@@ -1,16 +1,19 @@
 import time
-from typing import List
+from pathlib import Path
 
 from bs4 import BeautifulSoup
 from markdown.extensions.tables import TableExtension
 import markdown
 
-HTML_TEMPLATE = '.\\build\\html_template.html'
+
+ENCODING = 'UTF-8'
 BS4_PARSER = 'html.parser'
-IMG_ALT_MESSAGE = 'Please login Xing\'s SharePoint using your current browser to view this image.'
-IMG_HOST_URL = '//10.0.0.23/rd/Thermal&Adv Engineering/CAE/battery-regulator/images'
-LOGO_URL = '//10.0.0.23/rd/Thermal&Adv Engineering/CAE/battery-regulator/images/xm-logo.ico'
-QUERY = f'.png?{int(time.time())}'
+IMG_ALT_MESSAGE = 'Please login intranet.'
+HTML_TEMPLATE = 'build/html_template.html'
+IMG_HOST_URL = '//10.0.0.23/rd/Thermal&Adv Engineering/CAE/battery-regulator/images/'
+FAVICON_URL = '//10.0.0.23/rd/Thermal&Adv Engineering/CAE/battery-regulator/images/xm-logo.ico'
+TGT_DIR, IMG_EXT = '../images/', '.png'
+HTML_DIR, HTML_EXT = 'html', '.html'
 
 
 def convert_img_to_label(soup: BeautifulSoup):
@@ -23,48 +26,44 @@ def convert_img_to_label(soup: BeautifulSoup):
 
 def replace_img_link(line: str) -> str:
     if 'img alt="" src=' in line:
+        query = f'.png?{int(time.time())}'
         return line\
             .replace('alt=""', f'alt="{IMG_ALT_MESSAGE}"')\
-            .replace('../images', IMG_HOST_URL)\
-            .replace('.png', QUERY)
+            .replace(TGT_DIR, IMG_HOST_URL)\
+            .replace(IMG_EXT, query)
     return line
 
 
-def transform_md_to_html(path_md: str) -> str:
-    with open(path_md, 'r', encoding='UTF-8') as f:
+def transform_md_to_html(path_md: Path) -> str:
+    with path_md.open('r', encoding=ENCODING) as f:
         contents_md = f.read()
-    extensions = [
-        TableExtension(use_align_attribute=True),
-        'fenced_code'
-    ]
-    contents_html = markdown.markdown(
+    return markdown.markdown(
         contents_md,
         tab_length=2,
-        extensions=extensions
+        extensions=[TableExtension(use_align_attribute=True), 'fenced_code']
     )
-    return contents_html
 
 
-def main(md_paths: List[str]):
-    for path_md in md_paths:
-        with open(HTML_TEMPLATE, 'r') as f:
-            soup = BeautifulSoup(f, BS4_PARSER)
+def insert_title(soup: BeautifulSoup):
+    head = soup.find('head')
+    h1 = soup.find('h1')
+    title = soup.new_tag('title')
+    title.string = f'XING | CAE Documents | {h1.string}'
+    link = soup.new_tag('link', rel='icon', type='image/x-icon', href=FAVICON_URL)
+    head.insert(0, title)
+    head.insert(1, link)
 
-        body_str = replace_img_link(transform_md_to_html(path_md))
-        body_soup = BeautifulSoup(body_str, BS4_PARSER)
-        convert_img_to_label(body_soup)
-        soup.body.append(body_soup)
-        path_html = path_md\
-            .replace('.\\markdown\\', '.\\html\\')\
-                .replace('.md', '.html')
-        
-        head = soup.find('head')
-        h1 = soup.find('h1')
-        title = soup.new_tag('title')
-        title.string = ' | '.join(['XING', 'CAE Documents', h1.string])
-        link = soup.new_tag('link', rel='icon', type='image/x-icon', href=LOGO_URL)
-        head.insert(0, title)
-        head.insert(1, link)
 
-        with open(path_html, 'w', encoding='UTF-8') as f:
-            f.write(str(soup))
+def main(path_md: Path):
+    body_str = replace_img_link(transform_md_to_html(path_md))
+    body_soup = BeautifulSoup(body_str, BS4_PARSER)
+    convert_img_to_label(body_soup)
+
+    with Path(HTML_TEMPLATE).open('r', encoding=ENCODING) as f:
+        soup = BeautifulSoup(f, BS4_PARSER)
+    soup.body.append(body_soup)
+    insert_title(soup)
+
+    path_html = Path(HTML_DIR).joinpath(path_md.stem).with_suffix(HTML_EXT)
+    with path_html.open('w', encoding=ENCODING) as f:
+        f.write(str(soup))
